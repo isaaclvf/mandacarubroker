ca6d46fe076aee8b90533068e56f330be57f85ae
Isaac Farias <isaacfarias006@gmail.com>
2024-02-14 13:14:19 -0300

    feat: add more validations to request dto

--- src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
+++ src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
@@ -1,15 +1,22 @@
 package com.mandacarubroker.domain.stock;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record RequestStockDTO(
         @Pattern(regexp = "[A-Za-z]{3}\\d", message = "Symbol must be 3 letters followed by 1 number")
+        @NotBlank(message = "Stock symbol cannot be blank")
         String symbol,
+
         @NotBlank(message = "Company name cannot be blank")
+        // Alphanumeric characters and white spaces only
+        @Pattern(regexp = "[a-zA-Z\\d\\s]", message = "Company name has invalid characters")
         String companyName,
+
         @NotNull(message = "Price cannot be null")
+        @Min(value = 0, message = "Price must be a positive number")
         double price
 ) {
 }
